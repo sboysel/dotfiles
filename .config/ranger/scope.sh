9b45bd1d8f65b3ca74073694baa5fa2822b16f5e
Sam Boysel <sboysel@gmail.com>
2023-04-02 16:49:50 -0400

    [ranger] reset config to defaults

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -41,9 +41,9 @@ FILE_EXTENSION_LOWER="$(printf "%s" "${FILE_EXTENSION}" | tr '[:upper:]' '[:lowe
 ## Settings
 HIGHLIGHT_SIZE_MAX=262143  # 256KiB
 HIGHLIGHT_TABWIDTH=${HIGHLIGHT_TABWIDTH:-8}
-HIGHLIGHT_STYLE=${HIGHLIGHT_STYLE:-base16/solarized-dark}
+HIGHLIGHT_STYLE=${HIGHLIGHT_STYLE:-pablo}
 HIGHLIGHT_OPTIONS="--replace-tabs=${HIGHLIGHT_TABWIDTH} --style=${HIGHLIGHT_STYLE} ${HIGHLIGHT_OPTIONS:-}"
-PYGMENTIZE_STYLE=${PYGMENTIZE_STYLE:-solarized-dark}
+PYGMENTIZE_STYLE=${PYGMENTIZE_STYLE:-autumn}
 OPENSCAD_IMGSIZE=${RNGR_OPENSCAD_IMGSIZE:-1000,1000}
 OPENSCAD_COLORSCHEME=${RNGR_OPENSCAD_COLORSCHEME:-Tomorrow Night}
 
@@ -92,11 +92,6 @@ handle_extension() {
             ## Preview as csv conversion
             ## Uses: https://github.com/dilshod/xlsx2csv
             xlsx2csv -- "${FILE_PATH}" && exit 5
-            exit 1;;
-
-        ## Markdown
-        md)
-            mdcat "${FILE_PATH}" && exit 5
             exit 1;;
 
         ## HTML
@@ -159,31 +154,31 @@ handle_image() {
             exit 7;;
 
         ## Video
-        video/*)
-            # Thumbnail
-            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-            exit 1;;
+        # video/*)
+        #     # Thumbnail
+        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+        #     exit 1;;
 
         ## PDF
-        application/pdf)
-            pdftoppm -f 1 -l 1 \
-                     -scale-to-x "${DEFAULT_SIZE%x*}" \
-                     -scale-to-y -1 \
-                     -singlefile \
-                     -jpeg -tiffcompression jpeg \
-                     -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-                && exit 6 || exit 1;;
+        # application/pdf)
+        #     pdftoppm -f 1 -l 1 \
+        #              -scale-to-x "${DEFAULT_SIZE%x*}" \
+        #              -scale-to-y -1 \
+        #              -singlefile \
+        #              -jpeg -tiffcompression jpeg \
+        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+        #         && exit 6 || exit 1;;
 
 
         ## ePub, MOBI, FB2 (using Calibre)
-        application/epub+zip|application/x-mobipocket-ebook|\
-        application/x-fictionbook+xml)
-            # ePub (using https://github.com/marianosimone/epub-thumbnailer)
-            epub-thumbnailer "${FILE_PATH}" "${IMAGE_CACHE_PATH}" \
-                "${DEFAULT_SIZE%x*}" && exit 6
-            ebook-meta --get-cover="${IMAGE_CACHE_PATH}" -- "${FILE_PATH}" \
-                >/dev/null && exit 6
-            exit 1;;
+        # application/epub+zip|application/x-mobipocket-ebook|\
+        # application/x-fictionbook+xml)
+        #     # ePub (using https://github.com/marianosimone/epub-thumbnailer)
+        #     epub-thumbnailer "${FILE_PATH}" "${IMAGE_CACHE_PATH}" \
+        #         "${DEFAULT_SIZE%x*}" && exit 6
+        #     ebook-meta --get-cover="${IMAGE_CACHE_PATH}" -- "${FILE_PATH}" \
+        #         >/dev/null && exit 6
+        #     exit 1;;
 
         ## Font
         application/font*|application/*opentype)
@@ -208,40 +203,40 @@ handle_image() {
 
         ## Preview archives using the first image inside.
         ## (Very useful for comic book collections for example.)
-         application/zip|application/x-rar|application/x-7z-compressed|\
-             application/x-xz|application/x-bzip2|application/x-gzip|application/x-tar)
-             local fn=""; local fe=""
-             local zip=""; local rar=""; local tar=""; local bsd=""
-             case "${mimetype}" in
-                 application/zip) zip=1 ;;
-                 application/x-rar) rar=1 ;;
-                 application/x-7z-compressed) ;;
-                 *) tar=1 ;;
-             esac
-             { [ "$tar" ] && fn=$(tar --list --file "${FILE_PATH}"); } || \
-             { fn=$(bsdtar --list --file "${FILE_PATH}") && bsd=1 && tar=""; } || \
-             { [ "$rar" ] && fn=$(unrar lb -p- -- "${FILE_PATH}"); } || \
-             { [ "$zip" ] && fn=$(zipinfo -1 -- "${FILE_PATH}"); } || return
-        
-             fn=$(echo "$fn" | python -c "import sys; import mimetypes as m; \
-                     [ print(l, end='') for l in sys.stdin if \
-                       (m.guess_type(l[:-1])[0] or '').startswith('image/') ]" |\
-                 sort -V | head -n 1)
-             [ "$fn" = "" ] && return
-             [ "$bsd" ] && fn=$(printf '%b' "$fn")
-        
-             [ "$tar" ] && tar --extract --to-stdout \
-                 --file "${FILE_PATH}" -- "$fn" > "${IMAGE_CACHE_PATH}" && exit 6
-             fe=$(echo -n "$fn" | sed 's/[][*?\]/\\\0/g')
-             [ "$bsd" ] && bsdtar --extract --to-stdout \
-                 --file "${FILE_PATH}" -- "$fe" > "${IMAGE_CACHE_PATH}" && exit 6
-             [ "$bsd" ] || [ "$tar" ] && rm -- "${IMAGE_CACHE_PATH}"
-             [ "$rar" ] && unrar p -p- -inul -- "${FILE_PATH}" "$fn" > \
-                 "${IMAGE_CACHE_PATH}" && exit 6
-             [ "$zip" ] && unzip -pP "" -- "${FILE_PATH}" "$fe" > \
-                 "${IMAGE_CACHE_PATH}" && exit 6
-             [ "$rar" ] || [ "$zip" ] && rm -- "${IMAGE_CACHE_PATH}"
-             ;;
+        # application/zip|application/x-rar|application/x-7z-compressed|\
+        #     application/x-xz|application/x-bzip2|application/x-gzip|application/x-tar)
+        #     local fn=""; local fe=""
+        #     local zip=""; local rar=""; local tar=""; local bsd=""
+        #     case "${mimetype}" in
+        #         application/zip) zip=1 ;;
+        #         application/x-rar) rar=1 ;;
+        #         application/x-7z-compressed) ;;
+        #         *) tar=1 ;;
+        #     esac
+        #     { [ "$tar" ] && fn=$(tar --list --file "${FILE_PATH}"); } || \
+        #     { fn=$(bsdtar --list --file "${FILE_PATH}") && bsd=1 && tar=""; } || \
+        #     { [ "$rar" ] && fn=$(unrar lb -p- -- "${FILE_PATH}"); } || \
+        #     { [ "$zip" ] && fn=$(zipinfo -1 -- "${FILE_PATH}"); } || return
+        #
+        #     fn=$(echo "$fn" | python -c "import sys; import mimetypes as m; \
+        #             [ print(l, end='') for l in sys.stdin if \
+        #               (m.guess_type(l[:-1])[0] or '').startswith('image/') ]" |\
+        #         sort -V | head -n 1)
+        #     [ "$fn" = "" ] && return
+        #     [ "$bsd" ] && fn=$(printf '%b' "$fn")
+        #
+        #     [ "$tar" ] && tar --extract --to-stdout \
+        #         --file "${FILE_PATH}" -- "$fn" > "${IMAGE_CACHE_PATH}" && exit 6
+        #     fe=$(echo -n "$fn" | sed 's/[][*?\]/\\\0/g')
+        #     [ "$bsd" ] && bsdtar --extract --to-stdout \
+        #         --file "${FILE_PATH}" -- "$fe" > "${IMAGE_CACHE_PATH}" && exit 6
+        #     [ "$bsd" ] || [ "$tar" ] && rm -- "${IMAGE_CACHE_PATH}"
+        #     [ "$rar" ] && unrar p -p- -inul -- "${FILE_PATH}" "$fn" > \
+        #         "${IMAGE_CACHE_PATH}" && exit 6
+        #     [ "$zip" ] && unzip -pP "" -- "${FILE_PATH}" "$fe" > \
+        #         "${IMAGE_CACHE_PATH}" && exit 6
+        #     [ "$rar" ] || [ "$zip" ] && rm -- "${IMAGE_CACHE_PATH}"
+        #     ;;
     esac
 
     # openscad_image() {
@@ -307,14 +302,13 @@ handle_mime() {
                 local pygmentize_format='terminal'
                 local highlight_format='ansi'
             fi
-            highlight \
-                --style="base16/solarized-dark" \
-                --replace-tabs="8" --out-format="ansi" \
+            env HIGHLIGHT_OPTIONS="${HIGHLIGHT_OPTIONS}" highlight \
+                --out-format="${highlight_format}" \
                 --force -- "${FILE_PATH}" && exit 5
-            # env COLORTERM=8bit bat --color=always --style="plain" \
-            #     -- "${FILE_PATH}" && exit 5
-            # pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
-            #     -- "${FILE_PATH}" && exit 5
+            env COLORTERM=8bit bat --color=always --style="plain" \
+                -- "${FILE_PATH}" && exit 5
+            pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
+                -- "${FILE_PATH}" && exit 5
             exit 2;;
 
         ## DjVu
