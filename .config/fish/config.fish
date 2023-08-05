b82a0434744955285e05dd4990093e262bf283d1
Sam Boysel <sboysel@gmail.com>
2023-08-04 19:11:19 -0700

    [fish] simplify working dir in notes function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -320,17 +320,17 @@ end
 function n --description "n [title]"
     if not set -q argv[1]
         # search all notes for editing
-        nvim (printf %s\n (find $NOTES_REPO/notes -type f) | fzf)
+        nvim (printf %s\n (find $NOTES_REPO -type f) | fzf)
     else
         set REGEX "*$argv*.md"
-        set SEARCH_RESULTS (find $NOTES_REPO/notes -type f -name $REGEX)
+        set SEARCH_RESULTS (find $NOTES_REPO -type f -name $REGEX)
         echo $SEARCH_RESULTS
         if test (count $SEARCH_RESULTS) -gt 0
             # match title with existing notes
             nvim (printf %s\n $SEARCH_RESULTS | fzf)  
         else
             # new notes
-            nvim $NOTES_REPO/notes/(date +"%Y%m%d")_$argv.md
+            nvim $NOTES_REPO/(date +"%Y%m%d")_$argv.md
         end
     end
 end
