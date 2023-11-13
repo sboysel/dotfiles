67e36f456dc25bd3a310920723cad3ece7bd221a
Sam Boysel <sboysel@gmail.com>
2023-11-13 08:28:19 -0500

    [nvim] basic treesitter config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,3 +26,15 @@ require('lualine').setup({
 
 -- [[ imports // plugin config // nvim-tree ]]
 require("nvim-tree").setup()
+
+-- [[ imports // plugin config // nvim-treesitter ]]
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
