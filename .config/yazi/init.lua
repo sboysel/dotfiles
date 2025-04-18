e36f898660f31b7b175ef597ca8ee55d9f404ce4
Sam Boysel <sboysel@gmail.com>
2025-04-17 19:18:43 -0700

    [yazi] adding basic config

---
+++ .config/yazi/init.lua
@@ -0,0 +1,5 @@
+-- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}
